8408d8e8fde32ca36ad3f2a3837c637fae0f8c81
Jaelin Lee <jaelin215@users.noreply.github.com>
2025-06-01 13:43:46 +0000

    updated keys

--- .devcontainer/setup_aws_env.sh
+++ .devcontainer/setup_aws_env.sh
@@ -4,5 +4,5 @@ set -e
 echo "Setting AWS credentials..."
 echo "export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" >> ~/.bashrc
 echo "export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> ~/.bashrc
-echo "export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-east-1}" >> ~/.bashrc
+echo "export echo =${AWS_DEFAULT_REGION:-us-east-1}" >> ~/.bashrc
 source ~/.bashrc
